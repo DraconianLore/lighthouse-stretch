9dc5a8684c2c4252a6e8269747b448d1595cf272
Steven Wing <steven.wing82@gmail.com>
2019-04-18 18:13:04 -0700

    added editable input and random checks, added numbers on barsand alligned them to bottom

--- src/main.js
+++ src/main.js
@@ -1,34 +1,60 @@
 // Main JavaScript file in Lighthouse Labs Stretch Project - Steven Wing
 
 
-
 // Function signature to draw a bar chart
 const drawBarChart = function(data, options, element){
   let numberOfBars = data.length;
   let colourToUse = options.colour;
-  let singleBar;
-  let barWidth = options.width;
+  let barWidth = (options.width / data.length) * 1.5;
+  let barText = 'black';
+  // check for black chart
+  if (colourToUse == 'black') {
+    barText = '#66ffff';
+  }
 
+  // if no width provided assign width of 500px
+  let graphWidth = 500;
+  if (options.width) {
+    graphWidth = options.width;
+  }
+
+  // if no height provided allow height to be determined by the data
+  let graphHeight;
+  if (options.height) {
+    graphHeight = options.height;
+  }
+
+  // Generate bar graph shell
   let outerShell = document.createElement("div");
-  outerShell.style = 'display:flex; flex-wrap: nowrap; align-items: flex-end; width: 30%; justify-content: center; padding: 50px';
-  outerShell.align = 'center';
+  outerShell.style = 'display:flex; flex-wrap: nowrap; height: ' + graphHeight + 'px; width: ' + graphWidth + 'px; align-items: flex-end; justify-content: center; padding: 10px; border-style: solid; border-width: 5px; align: center';
   element.appendChild(outerShell);
 
+  // Generate bar graph
   for (let i = 0; i < numberOfBars; i++) {
-    singleBar = "<img src='" + colourToUse + ".png' height='"
-    + data[i] * 10 + "' width='" + barWidth + "'></img>";
-
+    // construct bars
     let barValue = document.createElement("div");
     barValue.style.width = barWidth + 'px';
     barValue.style.height = (data[i] * 10) + 'px';
     barValue.style.background = colourToUse;
+    barValue.style.display = 'table';
+
+    let barValueNumber = document.createElement('div');
+    barValueNumber.style = 'display: table-cell; vertical-align: bottom'
+    barValueNumber.style.color = barText;
+
+    barValueNumber.innerText = data[i];
+    barValue.appendChild(barValueNumber);
 
 
+    // construct devider for between bars
     let barDevider = document.createElement("div");
     barDevider.style.width = (barWidth / 2) + 'px';
     barDevider.style.background = "white";
     outerShell.appendChild(barValue);
-    outerShell.appendChild(barDevider);
 
+    // Add space between bars
+    if (i + 1 < numberOfBars){
+      outerShell.appendChild(barDevider);
+    }
   }
 }
