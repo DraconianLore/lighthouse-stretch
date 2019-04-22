3d49b619a3a1fa160b0d5079148782be4b0b2ef1
Steven Wing <steven.wing82@gmail.com>
2019-04-22 17:32:16 +0000

    made bars scale to the graph size correctly

--- src/main.js
+++ src/main.js
@@ -4,22 +4,30 @@
 // Function signature to draw a bar chart
 const drawBarChart = function(data, options, element){
   let numberOfBars = data.length;
-  let colourToUse = options.colour;
-  let barWidth = (options.width / data.length) * 1.5;
   let barText = 'black';
-  // check for black chart
+
+  // Set default colour to Blue if there is no colour option sent
+  let colourToUse = 'blue';
+  if (options.colour) {
+    colourToUse = options.colour;
+  }
+
+  //change text colour on bars if bar is set to black
   if (colourToUse == 'black') {
     barText = '#66ffff';
   }
 
-  // if no width provided assign width of 500px
-  let graphWidth = 500;
+  // if no width provided assign width of 300px
+  let graphWidth = 300;
   if (options.width) {
     graphWidth = options.width;
   }
 
+  //set bar width based on graph size
+  let barWidth = (graphWidth / data.length) * 1.5;
+
   // if no height provided allow height to be determined by the data
-  let graphHeight;
+  let graphHeight = 300;
   if (options.height) {
     graphHeight = options.height;
   }
@@ -29,19 +37,27 @@ const drawBarChart = function(data, options, element){
   outerShell.style = 'display:flex; flex-wrap: nowrap; height: ' + graphHeight + 'px; width: ' + graphWidth + 'px; align-items: flex-end; justify-content: center; padding: 10px; border-style: solid; border-width: 5px; align: center';
   element.appendChild(outerShell);
 
+  // Get largest bar size and make graph scalable
+  let largestBar = 0;
+    for(let i = 0; i < numberOfBars; i++) {
+      if (data[i] > largestBar) {
+        largestBar = data[i];
+      }
+    }
+  let gHeight = graphHeight / largestBar;
+
   // Generate bar graph
   for (let i = 0; i < numberOfBars; i++) {
     // construct bars
     let barValue = document.createElement("div");
     barValue.style.width = barWidth + 'px';
-    barValue.style.height = (data[i] * 10) + 'px';
+    barValue.style.height = (data[i] * gHeight) + 'px';
     barValue.style.background = colourToUse;
-    barValue.style.display = 'table';
+    barValue.style.display = 'table'; //breaks ff
 
     let barValueNumber = document.createElement('div');
     barValueNumber.style = 'display: table-cell; vertical-align: bottom'
     barValueNumber.style.color = barText;
-
     barValueNumber.innerText = data[i];
     barValue.appendChild(barValueNumber);
 
