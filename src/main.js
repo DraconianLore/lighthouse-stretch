b5e79d5e0f79fe76eee25238ca0a2f63612cd55c
Steven Wing <steven.wing82@gmail.com>
2019-05-10 08:45:50 +0000

    back to the start after missing 'stacked' in the instructions, forked my first attempt which was a multiset instead of stacked for reference

--- src/main.js
+++ src/main.js
@@ -11,268 +11,66 @@
                 alignment - alignment of the value inside the bar: top, center, bottom
                 titleColour - Colour of the Title text
                 titleSize - size of title font in pixels
+                titleBackground - Background colour for title
       element:  the element ID of the DOM element you want the bar placed into
 
-  For multiple value charts, first draw the first chart, then
-  call 'multiValueBarChart' function  with the same arguments as the initial bar.
-  Restrictions:
-    Second set of bars cannot have more data values than the first
-    Currently you may only have two sets of data per graph
 */
 
-// Draw initial bar chart
 const drawBarChart = function(data, options, element){
-  let numberOfBars = data.length;
-  // set text colour
-  let barText = 'white';
-    if (options.color) {
-    barText = options.color;
-  }
 
-  // Set default colour to Blue if there is no colour option sent
-  let colourToUse = 'blue';
-  if (options.backgroundColor) {
-    colourToUse = options.backgroundColor;
-  }
-
-
-  // if no width provided assign width of 300px
-  let graphWidth = 300;
-  if (options.width) {
-    graphWidth = options.width;
-  }
-
-  //set bar width based on graph size
-  let barWidth = (graphWidth / data.length) * 1.5;
-
-  // if no height provided allow height to be determined by the data
+  // check options and set defaults if empty
   let graphHeight = 300;
-  if (options.height) {
+  if (options.height){
     graphHeight = options.height;
   }
-  // generate graph title
-  let graphTitle = document.createElement('div');
-  graphTitle.style = 'display:flex; flex-wrap: nowrap; height: ' + (graphHeight / 5) + 'px; width: ' + graphWidth + 'px; align-items: center; justify-content: center; padding: 10px; align: center';
-  graphTitle.style.backgroundColor = '#eeeeee';
-  graphTitle.style.color = "black";
+  let graphWidth = 300;
+  if (options.width){
+    graphWidth = options.width;
+  }
+  let titleBG = '#eeeeee';
+  if (options.titleBackground) {
+    titleBG = options.titleBackground;
+  }
+  let titleColour = 'black';
   if (options.titleColour) {
-    graphTitle.style.color = options.titleColour;
+    titleColour = options.titleColour;
   }
-  graphTitle.style.fontSize = "36px";
+  let titleFontSize = '36px';
   if (options.titleSize) {
-    graphTitle.style.fontSize = options.titleSize + 'px';
-  }
-  graphTitle.innerHTML = '<b>' + options.title + '</b>';
-  element.appendChild(graphTitle);
-
-  graphHeight = graphHeight * 0.8;
-
-
-  // Generate bar graph shell
-  let outerShell = document.createElement("div");
-  outerShell.style = 'display:flex; flex-wrap: nowrap; height: ' + graphHeight + 'px; width: ' + graphWidth + 'px; align-items: flex-end; justify-content: center; padding: 10px; border-style: solid; border-width: 5px; align: center';
-  outerShell.id = 'outerShell';
-
-  element.appendChild(outerShell);
-
-  // Get largest bar size and make graph scalable
-  let largestBar = 0;
-    for(let i = 0; i < numberOfBars; i++) {
-      if (data[i] > largestBar) {
-        largestBar = data[i];
-      }
-    }
-  let gHeight = graphHeight / largestBar;
-  let barDelay = 0;
-
-   // Generate bar graph
-  for (let i = 0; i < numberOfBars; i++) {
-    // construct bars
-    let barValue = document.createElement("div");
-    barValue.style.display = 'flex';
-    barValue.style.flexDirection = "column-reverse";
-    barValue.style.justifyContent = "flex-end";
-    // check for alignment
-    if (options.alignment) {
-      switch(options.alignment) {
-        case 'center':
-          barValue.style.justifyContent = 'center';
-          break;
-        case 'top':
-          barValue.style.justifyContent = 'flex-end';
-          break;
-        case 'bottom':
-          barValue.style.justifyContent = 'flex-start';
-          break;
-      }
-    }
-
-    barValue.style.width = barWidth + 'px';
-    barValue.style.height = '0px';
-    barDelay += 200;
-
-    barValue.style.background = colourToUse;
-
-    barValue.id = 'bar' + i;
-    barValue.value = data[i];
-
-    let barValueNumber = document.createElement('div');
-    barValueNumber.style = 'display: flex; justify-content: center';
-    barValueNumber.style.color = barText;
-    barValueNumber.innerText = data[i];
-    barValue.appendChild(barValueNumber);
-
-
-    // construct devider for between bars
-    let barDevider = document.createElement("div");
-    barDevider.style.width = (barWidth / 3) + 'px';
-    barDevider.style.background = "white";
-
-    // insert the bar into the graph
-    outerShell.appendChild(barValue);
-
-    let transformHeight = (data[i] * gHeight) + 'px';
-
-    setTimeout(() => {
-      enlargeBar(transformHeight, 'bar' + i);
-    }, barDelay);
-
-    // Add space between bars
-    if (i + 1 < numberOfBars){
-      outerShell.appendChild(barDevider);
-    }
+    titleFontSize = options.titleSize + 'px';
   }
 
-  // Generate Labels
-  let labelShell = document.createElement("div");
-  labelShell.style = 'display:flex; height: ' + (  graphHeight / 2) + 'px; width: ' + graphWidth + 'px; justify-content: center; padding: 10px;';
-  labelShell.style.alignItems= 'flex-end';
+  // create title and container if this is the first data set
+  if (!document.getElementById('outerShell')) {
+    let chartTitle = generateFlexbox(graphHeight / 5, graphWidth);
+    chartTitle.style.alignItems = 'center';
+    chartTitle.style.backgroundColor = titleBG;
+    chartTitle.style.color = titleColour;
+    chartTitle.style.fontSize = titleFontSize;
+    chartTitle.innerHTML = '<b>' + options.title + '</b>';
+    element.appendChild(chartTitle);
 
-  element.appendChild(labelShell);
-
-  for (let i = 0; i < numberOfBars; i++) {
-    let barLabel = document.createElement("div");
-    barLabel.style = 'display: flex; align-items: center; justify-content: center'
-    barLabel.style.width = barWidth + 'px';
-    barLabel.style.height = (graphHeight / 2) + 'px';
-    barLabel.style.background = '#999999';
-
-    let barDevider = document.createElement("div");
-    barDevider.style.width = (barWidth / 2) + 'px';
-    barDevider.style.background = "white";
-
-    let labelText = document.createElement('div');
-    labelText.style = 'writing-mode: vertical-rl; text-orientation: sideways';
-    labelText.style.color = '#eeeeee';
-    labelText.innerText = 'Data Set ' + (i + 1); // As the function signature has no area set
-              // aside for Labels I have just named it simply. Would like to see it added with
-              // first bar sets number array, changing it to an object array in the future
-
-
-    labelShell.appendChild(barLabel);
-    barLabel.appendChild(labelText);
-    if (i + 1 < numberOfBars){
-      labelShell.appendChild(barDevider);
-    }
-
+    let outerShell = generateFlexbox(graphHeight * 0.8, graphWidth);
+    outerShell.id = 'outerShell';
+    outerShell.style.borderStyle = 'solid';
+    outerShell.style.borderWidth = '5px';
+    element.appendChild(outerShell);
   }
+
 
 }
 
-// Draw second data set on initial graph
-const multiValueBarChart = function(data, options, element) {
-  // Reduce width of existing bars and add the new bar beside it
-  let numberOfBars = data.length;
-
-  // Check if there is more data sent than existing graph and return error if there is
-  if (!document.getElementById('bar' + (numberOfBars - 1))) {
-    alert('Error: Second set of data cannot contain more values than existing graph \nPlease try again');
-    return;
-  }
-  if (document.getElementById('barTwo0')) {
-    alert('Error: Second data set already exists. \nYou may only have 2 sets of values per graph');
-    return;
-  }
-  // set colours for second bad and bar text
-  let secondColour = 'blue';
-  if (options.backgroundColor) {
-    secondColour = options.backgroundColor
-  }
-  // check if bars are the same colour and change if they are
-  if (secondColour == document.getElementById('bar0').style.backgroundColor) {
-    secondColour = 'purple';
-  }
-  let barText = 'white';
-  if (options.color) {
-    barText = options.color;
-  }
-
-  //set bar width based on graph size
-  let barWidth = parseInt(document.getElementById('bar0').style.width,10) / 2;
-  let largestBar = 0;
-
-  for (let i = 0; i < numberOfBars; i++) {
-    let thisBar = document.getElementById('bar' + i).value;
-
-    if (thisBar > largestBar) {
-      largestBar = thisBar;
-    }
-  }
-
-  let gHeight = (parseInt(element.style.height,10)) / largestBar;
-  let barDelay = 0;
-
-  for (let i = 0; i < numberOfBars; i++) {
-    let currentBar = document.getElementById('bar' + i);
-    currentBar.style.width =  barWidth + 'px';
-
-    barDelay += 200;
-
-    let barValue = document.createElement("div");
-    barValue.style.display = 'table';
-    barValue.style = "table-layout: fixed";
-    barValue.style.width = barWidth + 'px';
-    barValue.style.background = secondColour;
-    barValue.style.height = '0px';
-    barValue.id = 'barTwo' + i;
-
-    let barValueNumber = document.createElement('div');
-    barValue.style.display = 'flex';
-    barValue.style.flexDirection = "column-reverse";
-    barValue.style.justifyContent = "flex-end";
-    // check for alignment
-    if (options.alignment) {
-      switch(options.alignment) {
-        case 'center':
-          barValue.style.justifyContent = 'center';
-          break;
-        case 'top':
-          barValue.style.justifyContent = 'flex-end';
-          break;
-        case 'bottom':
-          barValue.style.justifyContent = 'flex-start';
-          break;
-      }
-    }
-    barValueNumber.style.color = barText;
-    barValue.appendChild(barValueNumber);
-
-    // set transform height, check if new bars are larger, if so set height to top of space
-    let transformHeight = (data[i] * gHeight) + 'px';
-    if (data[i] > largestBar) {
-      transformHeight = (largestBar * gHeight + 10) + 'px';
-      barValueNumber.style.fontWeight = 'bold';
-      barValueNumber.innerText = '^ \n';
-    }
-    barValueNumber.innerText += data[i];
-
-    currentBar.parentNode.insertBefore(barValue, currentBar.nextSibling);
-    setTimeout(() => {
-      enlargeBar(transformHeight, 'barTwo' + i);
-    }, barDelay);
-
-  }
+const generateFlexbox = function(flexHeight, flexWidth){
+    let flexy = document.createElement('div');
+    flexy.style.display = 'flex';
+    flexy.style.alignItems = 'flex-end';
+    flexy.style.justifyContent = 'center';
+    flexy.style.padding = '10px';
+    flexy.style.height = flexHeight + 'px';
+    flexy.style.width = flexWidth + 'px';
+    return flexy;
 }
+
 
 // set bars to enlarge
 const enlargeBar = function(newHeight, barToResize) {
