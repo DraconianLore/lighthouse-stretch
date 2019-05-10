5eb5fb5d6407ae794cc31cd66b06f2e679fb4a76
Steven Wing <steven.wing82@gmail.com>
2019-05-10 10:20:43 +0000

    created basic framework, outline of graph, graph title

--- src/main.js
+++ src/main.js
@@ -4,32 +4,33 @@
   Usage:
     drawBarChart (data, options, element)
       data: an array of numbers (the values to be put in the graph)
-      options:  backgroundColor - bar colour (default blue)
+      options:  height: height of the entire bar graph(default: 300px)
                 width - width of the entire bar graph(default: 300px)
-                height: height of the entire bar graph(default: 300px)
-                color - colour of the text inside the bars(default: white)
-                alignment - alignment of the value inside the bar: top, center, bottom
                 titleColour - Colour of the Title text
                 titleSize - size of title font in pixels
                 titleBackground - Background colour for title
+                backgroundColor - bar colour (default blue)
+                color - colour of the text inside the bars(default: white)
+                alignment - alignment of the value inside the bar: top, center, bottom
       element:  the element ID of the DOM element you want the bar placed into
 
 */
 
 const drawBarChart = function(data, options, element){
 
+  // get number of values in data
+  let numberOfBars = data.length;
+  // set current working bar set
+  let currenrBarSet;
+
   // check options and set defaults if empty
   let graphHeight = 300;
-  if (options.height){
+  if (options.height) {
     graphHeight = options.height;
   }
   let graphWidth = 300;
-  if (options.width){
+  if (options.width) {
     graphWidth = options.width;
-  }
-  let titleBG = '#eeeeee';
-  if (options.titleBackground) {
-    titleBG = options.titleBackground;
   }
   let titleColour = 'black';
   if (options.titleColour) {
@@ -39,9 +40,36 @@ const drawBarChart = function(data, options, element){
   if (options.titleSize) {
     titleFontSize = options.titleSize + 'px';
   }
+  let titleBG = '#eeeeee';
+  if (options.titleBackground) {
+    titleBG = options.titleBackground;
+  }
+  let barColour = 'blue';
+  if (options.backgroundColor) {
+    barColour = options.backgroundColor;
+  }
+  let barValueColour = 'white';
+  if (options.color) {
+    barValueColour = options.color;
+  }
+  let barValueAlignment = 'top';
+  if (options.alignment) {
+    switch(options.alignment) {
+      case 'center':
+        barValueAlignment = 'center';
+        break;
+      case 'top':
+        barValueAlignment = 'flex-end';
+        break;
+      case 'bottom':
+        barValueAlignment = 'flex-start';
+        break;
+    }
+  }
 
   // create title and container if this is the first data set
   if (!document.getElementById('outerShell')) {
+    // Generate title
     let chartTitle = generateFlexbox(graphHeight / 5, graphWidth);
     chartTitle.style.alignItems = 'center';
     chartTitle.style.backgroundColor = titleBG;
@@ -49,14 +77,53 @@ const drawBarChart = function(data, options, element){
     chartTitle.style.fontSize = titleFontSize;
     chartTitle.innerHTML = '<b>' + options.title + '</b>';
     element.appendChild(chartTitle);
-
+    // Generate Bargraph Shell
     let outerShell = generateFlexbox(graphHeight * 0.8, graphWidth);
     outerShell.id = 'outerShell';
     outerShell.style.borderStyle = 'solid';
     outerShell.style.borderWidth = '5px';
     element.appendChild(outerShell);
+  } else {
+    //check data sets are compatible or throw an error
+    if (!document.getElementById('barSet1.' + numberOfBars - 1).value) {
+      alert('Error: Cannot stack more data values than existing graph. \nPlease try again');
+    }
+    if (!document.getElementById('barSet5.1').value) {
+      alert('Error: Maximum sets of data is 5. \nPlease create a new graph')
+    }
   }
 
+  // Get largest bar and make graph scalable
+  let largestBar = 0;
+  // Largest in new data set
+  for (let i = 0; i < numberOfBars; i++) {
+    if (data[i] > largestBar) {
+      largestBar = data[i];
+    }
+  }
+  // Check for and add largest existing bar
+  for (let i = 0; i < 5; i++) {
+    if (document.getElementById('barSet' + i + '.1').value) {
+      let currentSetLargest = 0;
+      for (let x = 0; x < numberOfBars; x++) {
+        currentSetLargest = 0;
+        if (document.getElementById('barSet' + i + '.' + x).value > currentSetLargest) {
+          currentSetLargest = document.getElementById('barSet' + i + '.' + x).value;
+        }
+      }
+      largestBar += currentSetLargest;
+    } else {
+      currenrBarSet = i + 1;
+      break;
+    }
+  }
+  let barGraphHeight = (graphHeight * 0.8) / largestBar;
+
+  // Generate bars
+  for (let i = 0; i < numberOfBars; i++) {
+    // use currenrBarSet
+
+  }
 
 }
 
@@ -73,6 +140,6 @@ const generateFlexbox = function(flexHeight, flexWidth){
 
 
 // set bars to enlarge
-const enlargeBar = function(newHeight, barToResize) {
+const resizeBar = function(newHeight, barToResize) {
   $('#' + barToResize).animate({height: newHeight}, 1000);
 }
